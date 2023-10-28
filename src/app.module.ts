d3b4884e6c5236acd3265726d125403592abf6d8
jGiova <josue@sisbps.com.ar>
2023-10-28 14:49:48 -0300

    refact

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,34 +3,27 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
-import { GeolocationModule } from './geolocation/geolocation.module';
-import { Geolocation } from './geolocation/entities/geolocation.entity'
 import { EventosModule } from './eventos/eventos.module';
-
+import { Evento } from './eventos/entities/evento.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot ({
+    TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'database',
       port: 5432,
       database: 'postgres',
       username: 'postgres',
       password: 'postgres',
-      entities: [
-        Geolocation,
-        "src/modules/**/*.entity.{ts,js}"
-      ],
+      entities: [Evento, 'src/modules/**/*.entity.{ts,js}'],
       logging: true,
-      synchronize: false // SI ESTA EN TRUE BORRA LA BASE DE DATOS! DEJARLO EN FALSE
+      synchronize: false, // SI ESTA EN TRUE BORRA LA BASE DE DATOS! DEJARLO EN FALSE
     }),
-    GeolocationModule,
     EventosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
 
 // ADD new modules with this command:  npx @nestjs/cli g resource eventos   -> eventos is a example
