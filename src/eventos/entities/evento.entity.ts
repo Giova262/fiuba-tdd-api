8fd93f933559c626991aea4252b410b2dcaf8585
jGiova <josue@sisbps.com.ar>
2023-10-28 14:56:57 -0300

    setup table

--- src/eventos/entities/evento.entity.ts
+++ src/eventos/entities/evento.entity.ts
@@ -1,1 +1,22 @@
-export class Evento {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({name:'eventos'})
+export class Evento {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    details: string;
+  
+    @Column()
+    date: string;
+  
+    @Column()
+    title: string;
+  
+    @Column()
+    category: string;
+    
+    @Column()
+    place: string;
+}
