35f00eace5d87bfbf957ce3d3f44a75e6f85c13e
jGiova <josue@sisbps.com.ar>
2023-10-28 14:24:42 -0300

    add type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot ({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "body_backend_user",
+      "password": "body2668",
+      "database": "body_backend",
+      "entities": [
+        "src/modules/**/*.entity.{ts,js}"
+      ],
+      "synchronize": false // SI ESTA EN TRUE BORRA LA BASE DE DATOS! DEJARLO EN FALSE
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
